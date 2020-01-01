d61580b8851e8f31cb8b721f0f1fe840dddf6d61
00day0 <therandomuser11@gmail.com>
2020-01-01 21:27:32 +0100

    One-UI like settings actionbar
    AshutoshSundresh: Don't make it optional

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -28,6 +28,7 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toolbar;
@@ -53,6 +54,8 @@ public class SettingsHomepageActivity extends FragmentActivity {
     Context context;
     ImageView avatarView;
     UserManager mUserManager;
+    View homepageSpacer;
+    View homepageMainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,8 +95,27 @@ public class SettingsHomepageActivity extends FragmentActivity {
         showFragment(new TopLevelSettings(), R.id.main_content);
         ((FrameLayout) findViewById(R.id.main_content))
                 .getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        homepageSpacer = findViewById(R.id.settings_homepage_spacer);
+        homepageMainLayout = findViewById(R.id.main_content_scrollable_container);
+
+        if (!isHomepageSpacerEnabled() && homepageSpacer != null && homepageMainLayout != null) {
+            homepageSpacer.setVisibility(View.GONE);
+            setMargins(homepageMainLayout, 0,0,0,0);
+        }
     }
 
+    private boolean isHomepageSpacerEnabled() {
+        return true;
+    }
+
+    private static void setMargins (View v, int l, int t, int r, int b) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(l, t, r, b);
+            v.requestLayout();
+        }
+    }   
     private void showFragment(Fragment fragment, int id) {
         final FragmentManager fragmentManager = getSupportFragmentManager();
         final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -106,6 +128,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
         }
         fragmentTransaction.commit();
     }
+
 
     @VisibleForTesting
     void setHomepageContainerPaddingTop() {
