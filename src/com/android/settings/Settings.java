5ad31324bea9fde54af6b347d2855aeeaa5e74cf
zoldycc <sanjheevisv@palladiumos.com>
2021-08-31 07:21:11 +0530

    Revert "Settings: export OmniJaws settings activity"
    This reverts commit f9eaf2072f69ce10141e1dffec016c14e5b851bb.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -238,7 +238,6 @@ public class Settings extends SettingsActivity {
     public static class AtomicHubSettingsActivity extends SettingsActivity {}
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
     public static class SmartPixelsActivity extends SettingsActivity {}
-    public static class OmniJawsSettingsActivity extends SettingsActivity { /* empty */ }
     public static class GamingModeActivity extends SettingsActivity {}
 
     /**
